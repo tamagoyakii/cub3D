3bdb49cea870e78cb4bd12cc5d32fa94b36b3f4e
KimJiyoon <jjuni28@naver.com>
2022-11-19 16:52:50 +0900

    [Feat] cub3d.h game, mlx 구조체

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -4,4 +4,24 @@
 # include "../mlx/mlx.h"
 # include <math.h>
 
+# define KEY_ESC 53
+# define KEY_W 13
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+
+
+typedef struct s_mlx t_mlx;
+typedef struct s_map t_map;
+
+typedef struct s_game {
+	t_map *map;
+	t_mlx *mlx;
+} t_game;
+
+typedef struct s_mlx {
+	void	*mlx_ptr;
+	void	*mlx_win;
+} t_mlx;
+
 #endif
