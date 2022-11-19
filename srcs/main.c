98d7e369ef7424ef4562a84b713427a6e5cf11ce
tamagoyakii <rlawlgus4063@naver.com>
2022-11-19 17:15:37 +0900

    [Feat] 윈도우 닫는 함수 추가

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,13 @@
 #include "cub3d.h"
 
+int	close_win(t_mlx *mlx)
+{
+	mlx_clear_window(mlx->mlx_ptr, mlx->mlx_win);
+	mlx_destroy_window(mlx->mlx_ptr, mlx->mlx_win);
+	// printf("See you later!\n");
+	exit(0);
+}
+
 int main(void)
 {
 	return (0);
