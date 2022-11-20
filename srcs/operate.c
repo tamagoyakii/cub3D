afe5e133bd18d0f46c891ac64efffea0cca55de3
tamagoyakii <rlawlgus4063@naver.com>
2022-11-20 17:29:40 +0900

    [Refactor] norm 확인

--- srcs/operate.c
+++ srcs/operate.c
@@ -1,5 +1,5 @@
-# include "cub3d.h"
-# include <math.h>
+#include "cub3d.h"
+#include <math.h>
 
 void	rotate(t_vec *v, int opt)
 {
@@ -12,42 +12,46 @@ void	rotate(t_vec *v, int opt)
 	rot = ROT_SPEED / 100 * opt;
 	v->dir_x = dir_x * cos(rot) - v->dir_y * sin(rot);
 	v->dir_y = dir_x * sin(rot) + v->dir_y * cos(rot);
-    v->pln_x = pln_x * cos(rot) - v->pln_y * sin(rot);
-    v->pln_y = pln_x * sin(rot) + v->pln_y * cos(rot);
+	v->pln_x = pln_x * cos(rot) - v->pln_y * sin(rot);
+	v->pln_y = pln_x * sin(rot) + v->pln_y * cos(rot);
 }
 
-void	move_WS(t_vec *v, t_cub *c, int opt)
+void	move_ws(t_vec *v, t_cub *c, int opt)
 {
 	int	des_x;
 	int	des_y;
 
 	des_x = (int)(v->pos_x + v->dir_x * MOVE_SPEED / 100 * opt);
 	des_y = (int)(v->pos_y + v->dir_y * MOVE_SPEED / 100 * opt);
-	if (c->map[(int)v->pos_y][des_x] == 0) v->pos_x = des_x;
-	if (c->map[des_y][(int)v->pos_x] == 0) v->pos_y = des_y;
+	if (c->map[(int)v->pos_y][des_x] == 0)
+		v->pos_x = des_x;
+	if (c->map[des_y][(int)v->pos_x] == 0)
+		v->pos_y = des_y;
 }
 
-void	move_AD(t_vec *v, t_cub *c, int opt)
+void	move_ad(t_vec *v, t_cub *c, int opt)
 {
 	int	des_x;
 	int	des_y;
 
 	des_x = (int)(v->pos_x + v->pln_x * MOVE_SPEED / 100 * opt);
 	des_y = (int)(v->pos_y + v->pln_y * MOVE_SPEED / 100 * opt);
-	if (c->map[(int)v->pos_y][des_x] == 0) v->pos_x = des_x;
-	if (c->map[des_y][(int)v->pos_x] == 0) v->pos_y = des_y;
+	if (c->map[(int)v->pos_y][des_x] == 0)
+		v->pos_x = des_x;
+	if (c->map[des_y][(int)v->pos_x] == 0)
+		v->pos_y = des_y;
 }
 
 int	key_press(int keycode, t_game *g)
 {
 	if (keycode == KEY_W)
-		move_WS(g->vec, g->cub, 1);
+		move_ws(g->vec, g->cub, 1);
 	if (keycode == KEY_S)
-		move_WS(g->vec, g->cub, -1);
+		move_ws(g->vec, g->cub, -1);
 	if (keycode == KEY_A)
-		move_AD(g->vec, g->cub, -1);
+		move_ad(g->vec, g->cub, -1);
 	if (keycode == KEY_D)
-		move_AD(g->vec, g->cub, 1);
+		move_ad(g->vec, g->cub, 1);
 	if (keycode == KEY_R)
 		rotate(g->vec, -1);
 	if (keycode == KEY_L)
