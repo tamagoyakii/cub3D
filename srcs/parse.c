193cc499bb0575c9ba28bbc17794126e13b4f7d5
pearpearB <smilyoung@gmail.com>
2022-11-20 23:26:47 +0900

    [Feat] 이미지 경로 파싱 구현

--- srcs/parse.c
+++ srcs/parse.c
@@ -1,8 +1,7 @@
 #include "cub3d.h"
-#include "../gnl/get_next_line.h" //libft 추가하게 되면 strlen같은 함수가 겹칠 것 같아서 걱정이네요..?!
 #include <fcntl.h>
 
-int	check_file(char *av)
+static int	check_file(char *av)
 {
 	int	fd;
 	int len;
@@ -10,37 +9,38 @@ int	check_file(char *av)
 	len = ft_strlen(av);
 	if (len < 4 || av[len - 4] != '.' || av[len - 3] != 'c'\
 		|| av[len - 2] != 'u' || av[len - 1] != 'b')
-		err_exit("Invalid file name!", NULL);
+		err_exit("Invalid file name.", NULL);
 	fd = open(av, O_RDONLY);
 	if (fd < 0)
 		err_exit("File open failed.", NULL);
 	return (fd);
 }
 
-void	init_map(t_cub *c)
+static void	init_cub(t_cub *c)
 {
-	(void ) c; //warning으로 잠시 추가 추후 삭제 바랍니다 🙌
-	// map 구조체 초기화하는 함수
+	c->no = NULL;
+	c->so = NULL;
+	c->we = NULL;
+	c->ea = NULL;
+	c->fl = -1;
+	c->ce = -1;
+	c->h = -1;
+	c->w = -1;
 }
 
-void	check_map(int fd)
-{
-	(void) fd;  //warning으로 잠시 추가 추후 삭제 바랍니다 🙌
-	// 파일 내용이 유효한지 확인하는 함수
-}
-
-void	parse_map(t_cub *c)
+static void	parse_map(t_cub *c)
 {
 	(void ) c; //warning으로 잠시 추가 추후 삭제 바랍니다 🙌
+	//  c->w, c->h도 저장해야 함
 	// 이차원 배열로 저장하는 함수
 }
 
 void	parse(char *av, t_cub *c)
 {
-	int		fd;
+	int				fd;
 
 	fd = check_file(av);
-	init_map(c);
-	check_map(fd);
+	init_cub(c);
+	parse_cub(fd, c);
 	parse_map(c);
 }
---
+++ srcs/parse_cub.c
@@ -0,0 +1,68 @@
+#include "../gnl/get_next_line.h" 
+#include "cub3d.h"
+#include <fcntl.h>
+
+static int set_cub_content(char *key, char *value, unsigned int len, t_cub *c)
+{
+	 // ft_strdup에 실패할 경우도 생각해봐야할 것 같아요 😭
+	if (ft_strncmp(key, "NO", len) == 0 && c->no == NULL)
+		c->no = ft_strdup(value);
+	else if (ft_strncmp(key, "SO", len) == 0 && c->so == NULL)
+		c->so = ft_strdup(value);
+	else if (ft_strncmp(key, "WE", len) == 0 && c->we == NULL)
+		c->we = ft_strdup(value);
+	else if (ft_strncmp(key, "EA", len) == 0 && c->ea == NULL)
+		c->ea = ft_strdup(value);
+	else if (ft_strncmp(key, "F", len) == 0 && c->fl == -1)
+		c->fl = 1; // 임시
+	else if (ft_strncmp(key, "C", len) == 0 && c->ce == -1)
+		c->ce = 1; // 임시
+	else
+		return (0);
+	return (1);
+}
+
+static void	set_cub(char ***split, char **line, t_cub *c)
+{
+	if (!set_cub_content((*split)[0], (*split)[1], ft_strlen((*split)[0]), c))
+	{
+		free(*line);
+		free_strs(*split);
+		err_exit("Malloc failed.", NULL);
+	}
+	else
+	{
+		free(*line);
+		free_strs(*split);
+	}
+}
+
+static void	check_cub(int fd, t_cub *c)
+{
+	char	*line;
+	char	**split;	
+
+	line = get_next_line(fd);
+	if (!line)
+		err_exit("Not enough file contents.", NULL);
+	if (is_empty_line(line))
+	{
+		free(line);
+		return ;
+	}
+	split = ft_split(line, ' ');
+	if (!split)
+	{
+		free(line);
+		err_exit("Malloc failed.", NULL);
+	}
+	set_cub(&split, &line, c);
+}
+
+void	parse_cub(int fd, t_cub *c)
+{
+	do {
+		check_cub(fd, c);
+	} while (c->no == NULL || c->so == NULL || c->we == NULL\
+			|| c->ea == NULL || c->fl == -1 || c->ce == -1);
+}
