44aa9ef7530ad1c633d52e3ed445782729311dff
KimJiyoon <jjuni28@naver.com>
2022-11-22 02:38:38 +0900

    [Feat] floor, ceil buf에 초기화

--- srcs/draw.c
+++ srcs/draw.c
@@ -65,6 +65,17 @@ void	dda(t_ray* r, t_cub *cub)
 	}
 }
 
+void draw_floor_ceil(t_game *g)
+{
+	int	i;
+
+	i = -1;
+	while (++i < WIN_Y / 2)
+		ft_memset(g->mlx->tmp[i], g->cub->ce, sizeof(int) * WIN_X);
+	i--;
+	while (++i < WIN_Y)
+		ft_memset(g->mlx->tmp[i], g->cub->fl, sizeof(int) * WIN_X);
+}
 
 void	raycasting(t_game *g)
 {
@@ -72,7 +83,7 @@ void	raycasting(t_game *g)
 	t_ray	r;
 
 	x = -1;
-	// draw_floor_ceil();
+	draw_floor_ceil(g);
 	while (++x < WIN_X)
 	{
 		calc_ray(g->vec, &r, x);
