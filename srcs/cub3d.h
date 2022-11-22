2ba8287fee14b8af69c06dd55d8f88d54513d7d3
tamagoyakii <rlawlgus4063@naver.com>
2022-11-22 19:29:59 +0900

    [Feat] error enum 추가

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -4,15 +4,22 @@
 # include "../mlx/mlx.h"
 # include "../libft/libft.h"
 
-// #define MOVE_SPEED	0.07;
-// #define TURN_SPEED	0.06;
+typedef enum e_return {
+	SUCCESS,
+	FAIL
+}	t_return;
+
+typedef enum e_error {
+	E_INIT	= 0x01,
+	E_PARSE	= 0x02,
+}	t_error;
 typedef enum e_keycode {
-	KEY_W = 13,
-	KEY_A = 0,
-	KEY_S = 1,
-	KEY_D = 2,
-	KEY_R = 39,
-	KEY_L = 37,
+	KEY_W	= 13,
+	KEY_A	= 0,
+	KEY_S	= 1,
+	KEY_D	= 2,
+	KEY_R	= 39,
+	KEY_L	= 37,
 	KEY_ESC = 53,
 }	t_keycode;
 
@@ -31,27 +38,23 @@ typedef enum e_speed {
 	MOVE_SPEED = 7,
 	ROT_SPEED = 6
 }	t_speed;
-// 열거형은 정수만 사용 가능하기 때문에
-// 8,9번째 줄처럼 #define 메크로로 사용할지
-// 열거형으로 선언하여 (MOVE_SPEED / 100) 할지.. 고민
-// 근데 메크로 사용해보니까 계산이 안돼서 열거형 사용중
 
 typedef struct s_ray {
 	double	cam_x;
 	double	raydir_x;
 	double	raydir_y;
-	int			map_x;
-	int			map_y;
+	int		map_x;
+	int		map_y;
 	double	deltadist_x;
 	double	deltadist_y;
 	double	sidedist_x;
 	double	sidedist_y;
-	int			step_x;
-	int			step_y;
-	int			hit;
-	int			side; //NS 1 or EW 0 ?
+	int		step_x;
+	int		step_y;
+	int		hit;
+	int		side; //NS 1 or EW 0 ?
 	double	wall_dist;
-} t_ray;
+}	t_ray;
 
 typedef struct s_mlx {
 	void	*mlx_ptr;
@@ -90,29 +93,41 @@ typedef struct s_game {
 /**********
 * main.c  *
 **********/
-void	err_exit(const char *str, t_game *game);
+void	err_exit(const char *str, t_game *g, int err);
 int		close_win(t_mlx *mlx);
 
-/**********
-* move.c  *
-**********/
+/*********
+* init.c *
+*********/
+int		init_struct(t_game *g);
+
+/*********
+* free.c *
+*********/
+void	free_double_ptr(void **ptr);
+void	free_cub(t_cub *cub);
+
+/*********
+* move.c *
+*********/
 int		key_press(int keycode, t_game *g);
 
-/**********
-* draw.c  *
-**********/
+/*********
+* draw.c *
+*********/
 int		draw_game(t_game *g);
 
 /**********
-* parse.c  *
+* parse.c *
 **********/
-void	free_strs(char **strs);
-int		is_empty_line(char *line);
-void	parse(char *av, t_cub *c);
-void	parse_cub(int fd, t_cub *c);
-void	parse_map(int fd, t_cub *c);
+int		parse(char *av, t_cub *c);
+int		parse_cub(int fd, t_cub *c);
+int		parse_map(int fd, t_cub *c);
 int		set_color(int *content, char *value);
+
+void	free_strs(char **strs);
 void	p_err_exit(const char *str, char **f_str);
+int		is_empty_line(char *line);
 
 /*************
 * init_mlx.c *
@@ -121,8 +136,8 @@ void	init_mlx_strt(t_game *g);
 
 /***************
 * raycasting.c *
-****************/
+***************/
 void	calc_ray(t_vec *v, t_ray *r, int x);
-void	dda(t_ray* r, t_cub *cub);
+void	dda(t_ray *r, t_cub *cub);
 
 #endif
