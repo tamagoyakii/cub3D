59c3f40c4d4865551575f68a18f1009bd90db2fe
KimJiyoon <jjuni28@naver.com>
2022-11-23 14:43:21 +0900

    [Fix] keycode 변경 및 operate.c 수정

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -3,7 +3,6 @@
 
 # include "../mlx/mlx.h"
 # include "../libft/libft.h"
-# define PI 3.141592
 
 typedef enum e_return {
 	SUCCESS	= 0,
@@ -20,7 +19,7 @@ typedef enum e_keycode {
 	KEY_A	= 0,
 	KEY_S	= 1,
 	KEY_D	= 2,
-	KEY_R	= 39,
+	KEY_R	= 124,
 	KEY_L	= 123,
 	KEY_ESC = 53,
 }	t_keycode;
--- srcs/operate.c
+++ srcs/operate.c
@@ -10,12 +10,11 @@ void	rotate(t_vec *v, int opt)
 
 	dir_x = v->dir_x;
 	pln_x = v->pln_x;
-	rot = ROT_SPEED / 100 * opt;
+	rot = (double)ROT_SPEED / 100 * opt;
 	v->dir_x = dir_x * cos(rot) - v->dir_y * sin(rot);
 	v->dir_y = dir_x * sin(rot) + v->dir_y * cos(rot);
 	v->pln_x = pln_x * cos(rot) - v->pln_y * sin(rot);
 	v->pln_y = pln_x * sin(rot) + v->pln_y * cos(rot);
-	printf("rotate dir x %f  dir y %f\n", v->dir_x, v->dir_y);
 }
 
 void	move_ws(t_vec *v, t_cub *c, int opt)
@@ -23,11 +22,11 @@ void	move_ws(t_vec *v, t_cub *c, int opt)
 	int	des_x;
 	int	des_y;
 
-	des_x = (int)(v->pos_x + v->dir_x * MOVE_SPEED / 100 * opt);
-	des_y = (int)(v->pos_y + v->dir_y * MOVE_SPEED / 100 * opt);
-	if (c->map[(int)v->pos_y][des_x] == 0)
+	des_x = (int)(v->pos_x + v->dir_x * (double)MOVE_SPEED / 100 * opt);
+	des_y = (int)(v->pos_y + v->dir_y * (double)MOVE_SPEED / 100 * opt);
+	if (c->map[(int)v->pos_y][des_x] == '0')
 		v->pos_x = des_x;
-	if (c->map[des_y][(int)v->pos_x] == 0)
+	if (c->map[des_y][(int)v->pos_x] == '0')
 		v->pos_y = des_y;
 }
 
@@ -36,11 +35,11 @@ void	move_ad(t_vec *v, t_cub *c, int opt)
 	int	des_x;
 	int	des_y;
 
-	des_x = (int)(v->pos_x + v->pln_x * MOVE_SPEED / 100 * opt);
-	des_y = (int)(v->pos_y + v->pln_y * MOVE_SPEED / 100 * opt);
-	if (c->map[(int)v->pos_y][des_x] == 0)
+	des_x = (int)(v->pos_x + v->pln_x * (double)MOVE_SPEED / 100 * opt);
+	des_y = (int)(v->pos_y + v->pln_y * (double)MOVE_SPEED / 100 * opt);
+	if (c->map[(int)v->pos_y][des_x] == '0')
 		v->pos_x = des_x;
-	if (c->map[des_y][(int)v->pos_x] == 0)
+	if (c->map[des_y][(int)v->pos_x] == '0')
 		v->pos_y = des_y;
 }
 
@@ -56,10 +55,9 @@ int	key_press(int keycode, t_game *g)
 	if (keycode == KEY_D)
 		move_ad(g->vec, g->cub, 1);
 	if (keycode == KEY_R)
-		rotate(g->vec, - PI / 6);
-	if (keycode == KEY_L){
-		rotate(g->vec, PI / 6);
-	}
+		rotate(g->vec, -1);
+	if (keycode == KEY_L)
+		rotate(g->vec, 1);
 	if (keycode == KEY_ESC)
 		close_win(g->mlx);
 	// 맵 프린트
